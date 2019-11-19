acd76b19baea29367e9115da33285a756b68b42d
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-11-19 16:33:09 +0800

    code refactoring & testing

--- main_top_down_baseline_addemb.py
+++ main_top_down_baseline_addemb.py
@@ -13,14 +13,14 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, mode
     print_freq = 400
     dev_score_list = []
 
-    if gpu_mode >= 0 :
+    '''if gpu_mode >= 0 :
         ngpus = 2
         device_array = [i for i in range(0,ngpus)]
 
         pmodel = torch.nn.DataParallel(model, device_ids=device_array)
     else:
-        pmodel = model
-    #pmodel = model
+        pmodel = model'''
+    pmodel = model
 
     top1 = imsitu_scorer.imsitu_scorer(encoder, 1, 3)
     top5 = imsitu_scorer.imsitu_scorer(encoder, 5, 3)
